1ba7b43b57e174b990d83adb8b182997bcb7c7db
rkotkas <redi.kotkas@gmail.com>
2024-07-10 14:19:40 +0200

    version 3

--- dev/pipeline.py
+++ dev/pipeline.py
@@ -1,9 +1,12 @@
+import pandas as pd
 import sqlalchemy
-from sqlalchemy import create_engine, inspect, MetaData, Table, Column, Integer, String, DateTime, Float, text
+from sqlalchemy import create_engine, text, inspect, MetaData, Table, Column, Integer, String, Float
 import json
 import logging
-import pandas as pd
 import os
+import unittest
+import sys
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -26,145 +29,244 @@ errorlog.addHandler(errorlog_handler)
 
 
 # Database connection details
-DB_URL = "sqlite:///cademycode.db"
+db_url = "sqlite:///cademycode.db"
+row_counts_file = 'row_counts.json'
 
-# create a database connection
-def create_db_connection(db_url):
-    return create_engine(db_url)
+# Create a database engine
+def create_db_engine(db_url):
+	return create_engine(db_url)
 
 
-# Function to get row counts for all tables
+def run_tests():
+    """Run the unittests in the tests directory."""
+    test_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'tests')
+    loader = unittest.TestLoader()
+    tests = loader.discover(start_dir=test_dir)
+    testRunner = unittest.TextTestRunner()
+    result = testRunner.run(tests)
+    return result.wasSuccessful()
+
+
+# get table names from database
+def get_table_names(engine):
+	inspector = inspect(engine)
+	table_names = inspector.get_table_names()
+	return table_names
+
+
 def get_row_counts(engine):
-    with engine.connect() as connection:
-        inspector = inspect(engine)
-        tables = inspector.get_table_names()
-        row_counts = {}
-        for table in tables:
-            result = connection.execute(text(f"SELECT COUNT(*) FROM {table}")).scalar()
-            row_counts[table] = result
-        return row_counts
+	with engine.connect() as connection:
+		inspector = inspect(engine)
+		tables = inspector.get_table_names()
+		row_counts = {}
+		for table in tables:
+			try:
+				result = connection.execute(text(f"SELECT COUNT(*) FROM {table}")).scalar()
+				row_counts[table] = result
+				changelog.info(f"Row count for table '{table}': {result}")
+			except Exception as e:
+				changelog.error(f"Error retrieving row count for table '{table}': {str(e)}")
+		return row_counts
 
 
 # Function to check for database updates based on row counts
-def check_for_updates(engine, last_row_counts):
-    current_row_counts = get_row_counts(engine)
-    for table, count in current_row_counts.items():
-        if table not in last_row_counts or last_row_counts[table] != count:
-            return True
-    return False
+def check_for_updates(engine, last_row_counts, current_row_counts=None):
+	if current_row_counts is None:
+		current_row_counts = get_row_counts(engine)
+
+	changelog.info("Checking for updates...")
+	changelog.info(f"Last row counts: {last_row_counts}")
+	changelog.info(f"Current row counts: {current_row_counts}")
+
+	for table, count in current_row_counts.items():
+		if table not in last_row_counts or last_row_counts[table] != count:
+			changelog.info(f"Update found in table '{table}': last count = {last_row_counts.get(table)}, current count = {count}")
+			return True
+	changelog.info("No updates found.")
+	return False
 
 
-# Function to update the database
-def update_database(engine):
-    inspector = inspect(engine)
-    tables = inspector.get_table_names()
-    dataframes = {}
-    if not tables:
-        logging.warning("No tables found in the database.")
+# Function to save row counts to a JSON file
+def save_row_counts(row_counts, file_path):
+    with open(file_path, 'w') as file:
+        json.dump(row_counts, file)
+
+
+# Function to load row counts from a JSON file
+def load_row_counts(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            return json.load(file)
     else:
-        logging.info("Tables in the database:")
-        for table in tables:
-            logging.info(table)
-            df = pd.read_sql_table(table, engine)
-            dataframes[table] = df
-            logging.info(f"Data from {table}:")
-            logging.info(df.head())
-    return dataframes
+        return {}
 
 
-# Function to process DataFrames
+# Function to get dataframes for all tables
+def get_dataframes(engine):
+	table_names = get_table_names(engine)
+	dataframes = {}
+	if not table_names:
+		changelog.warning("No tables found in the database.")
+	else:
+		changelog.info("Tables in the database:")
+		for table_name in table_names:
+			changelog.info(table_name)
+			df = pd.read_sql_table(table_name, engine)
+			dataframes[table_name] = df
+			changelog.info(f"Data from {table_name}:")
+			changelog.info(df.head().to_string())  # Convert DataFrame head to string
+	return dataframes
+
+
+# remove NaN and duplictes
 def process_dataframes(dataframes):
-    df_students = dataframes.get('cademycode_students', pd.DataFrame())
-    df_jobs = dataframes.get('cademycode_student_jobs', pd.DataFrame())
-    df_courses = dataframes.get('cademycode_courses', pd.DataFrame())
+	# Clean and process df_students
+	df_students = dataframes.get('cademycode_students', pd.DataFrame())
+	df_jobs = dataframes.get('cademycode_student_jobs', pd.DataFrame())
+	df_courses = dataframes.get('cademycode_courses', pd.DataFrame())
 
-    # Clean and process df_students
-    df_students['job_id'] = pd.to_numeric(df_students['job_id'], errors='coerce')
-    df_students['num_course_taken'] = pd.to_numeric(df_students['num_course_taken'], errors='coerce')
-    df_students['time_spent_hrs'] = pd.to_numeric(df_students['time_spent_hrs'], errors='coerce')
-    df_students['current_career_path_id'] = pd.to_numeric(df_students['current_career_path_id'], errors='coerce')
-    
-    df_students_clean = df_students.dropna(subset=['job_id', 'num_course_taken', 'current_career_path_id', 'time_spent_hrs'])
+	df_students['job_id'] = pd.to_numeric(df_students['job_id'], errors='coerce')
+	df_students['num_course_taken'] = pd.to_numeric(df_students['num_course_taken'], errors='coerce')
+	df_students['time_spent_hrs'] = pd.to_numeric(df_students['time_spent_hrs'], errors='coerce')
+	df_students['current_career_path_id'] = pd.to_numeric(df_students['current_career_path_id'], errors='coerce')
 
-    df_students_clean.loc[:, 'job_id'] = df_students_clean['job_id'].astype(int)
-    df_students_clean.loc[:, 'num_course_taken'] = df_students_clean['num_course_taken'].astype(int)
-    df_students_clean.loc[:, 'time_spent_hrs'] = df_students_clean['time_spent_hrs'].astype(float)
-    df_students_clean.loc[:, 'contact_info'] = df_students_clean['contact_info'].apply(json.dumps)
-    df_students_clean.loc[:, 'current_career_path_id'] = df_students_clean['current_career_path_id'].astype(int)
-    
-    # Drop duplicates from df_jobs
-    df_jobs_clean = df_jobs.drop_duplicates()
-    
-    logging.info("df_students NaN values and df_jobs duplicates dropped")
+	df_students_clean = df_students.copy()
+	nan_counts = df_students_clean.isna().sum()
 
-    return df_students_clean, df_jobs_clean, df_courses
+	# Drop rows with NaN values
+	if (nan_counts != 0).any():
+		changelog.info(f"Dropping df_students NaN values {nan_counts}")
+		df_students_clean = df_students.dropna(subset=['job_id', 'num_course_taken', 'current_career_path_id', 'time_spent_hrs']).copy()
+		changelog.info("NaN values dropped")
+	else:
+		changelog.info("No NaN values found in df_students")
+
+	df_students_clean['job_id'] = df_students_clean['job_id'].astype(int)
+	df_students_clean['num_course_taken'] = df_students_clean['num_course_taken'].astype(int)
+	df_students_clean['time_spent_hrs'] = df_students_clean['time_spent_hrs'].astype(float)
+	df_students_clean['contact_info'] = df_students_clean['contact_info'].apply(json.dumps)
+	df_students_clean['current_career_path_id'] = df_students_clean['current_career_path_id'].astype(int)
+
+	# Drop duplicated values
+	df_jobs_clean = df_jobs.copy()
+	jobs_duplicates = df_jobs.duplicated()
+	if jobs_duplicates.any():
+		changelog.info(f"Dropping df_jobs duplicates {df_jobs.duplicated()}")
+		df_jobs_clean = df_jobs.drop_duplicates()
+		changelog.info("Duplicates dropped")
+	else:
+		changelog.info("No duplicates found in df_jobs")
+
+	changelog.info("Dataframes processing completed")
+
+	return df_students_clean, df_jobs_clean, df_courses
 
 
 # Function to save DataFrames back to the database
-def update_db_table(engine, df_students_clean):
-    metadata = MetaData()
-    cademycode_students2 = Table(
-        'cademycode_students2',
-        metadata,
-        Column('uuid', Integer, primary_key=True),
-        Column('name', String),
-        Column('dob', String),
-        Column('sex', String),
-        Column('contact_info', String),
-        Column('job_id', Integer),
-        Column('num_course_taken', Integer),
-        Column('current_career_path_id', Integer),
-        Column('time_spent_hrs', Float)
-    )
+def update_db_tables(engine, df_students, df_jobs, df_courses):
+	metadata = MetaData()
 
-    # Create new table
-    metadata.create_all(engine)
+	cademycode_students2 = Table(
+		'cademycode_students2',
+		metadata,
+		Column('uuid', Integer, primary_key=True),
+		Column('name', String),
+		Column('dob', String),
+		Column('sex', String),
+		Column('contact_info', String),
+		Column('job_id', Integer),
+		Column('num_course_taken', Integer),
+		Column('current_career_path_id', Integer),
+		Column('time_spent_hrs', Float)
+	)
 
-    # Insert data into new table
-    df_students_clean.to_sql('cademycode_students2', engine, if_exists='replace', index=False)
-  
-    # Drop old table and rename new table
-    with engine.connect() as conn:
-        drop_statement = text("DROP TABLE IF EXISTS cademycode_students")
-        conn.execute(drop_statement)
-        rename_statement = text("ALTER TABLE cademycode_students2 RENAME TO cademycode_students")
-        conn.execute(rename_statement)
-    changelog.info('Database updated successfully.')
+	cademycode_jobs2 = Table(
+		'cademycode_jobs2',
+		metadata,
+		Column('job_id', Integer, primary_key=True),
+		Column('job_category', String),
+		Column('avg_salary', Integer)
+	)
+
+	cademycode_courses2 = Table(
+		'cademycode_courses2',
+		metadata,
+		Column('career_path_id', Integer, primary_key=True),
+		Column('career_path_name', String),
+		Column('hours_to_complete', Integer)
+	)
+
+	with engine.begin() as conn:
+		try:
+			# Create new tables
+			metadata.create_all(conn)
+
+			# Insert data into new table
+			df_students.to_sql('cademycode_students2', engine, if_exists='replace', index=False)
+			df_jobs.to_sql('cademycode_jobs2', engine, if_exists='replace', index=False)
+			df_courses.to_sql('cademycode_courses2', engine, if_exists='replace', index=False)
+
+			# Drop old table and rename new table
+			drop_cademycode_students = text("DROP TABLE IF EXISTS cademycode_students")
+			drop_cademycode_jobs = text("DROP TABLE IF EXISTS cademycode_student_jobs")
+			drop_cademycode_courses = text("DROP TABLE IF EXISTS cademycode_courses")
+
+			conn.execute(drop_cademycode_students)
+			conn.execute(drop_cademycode_jobs)
+			conn.execute(drop_cademycode_courses)
+
+			rename_drop_cademycode_students = text("ALTER TABLE cademycode_students2 RENAME TO cademycode_students")
+			rename_drop_cademycode_jobs = text("ALTER TABLE cademycode_jobs2 RENAME TO cademycode_student_jobs")
+			rename_drop_cademycode_courses = text("ALTER TABLE cademycode_courses2 RENAME TO cademycode_courses")
+
+			conn.execute(rename_drop_cademycode_students)
+			conn.execute(rename_drop_cademycode_jobs)
+			conn.execute(rename_drop_cademycode_courses)
+
+			changelog.info('Database updated successfully.')
+
+		except Exception as e:
+			changelog.error(f"Error updating database: {e}")
+			raise
 
 
-# Merge DataFrames
-def merge_df(df_students, df_jobs_clean, df_courses):
-    merged_df = pd.merge(df_students, df_jobs_clean, on='job_id', how='inner')
-    changelog.info('df_students and df_jobs_clean merged')
+def merge_df(df_students, df_jobs, df_courses):
+	merged_df = df_students.merge(df_jobs, on='job_id', how='inner')
+	changelog.info('df_students and df_jobs merged')
 
-    merged_df = pd.merge(merged_df, df_courses, left_on='current_career_path_id', right_on='career_path_id', how='inner')
-    changelog.info('df_courses merged to df_students_clean')
+	merged_df = merged_df.merge(df_courses, left_on='current_career_path_id', right_on='career_path_id', how='inner')
+	changelog.info('df_courses merged to merged_df')
+	return merged_df
 
-    return merged_df
-
-
-# Main function
 def main():
-    engine = create_db_connection(DB_URL)
-    last_row_counts = {
-        "cademycode_courses": 1,
-        "cademycode_student_jobs": 13,
-        "cademycode_students": 5000
-    }
-    try:
-        if not check_for_updates(engine, last_row_counts):
-            logging.info("No updates found in the database.")
-        else:
-            logging.info("Updates found in the database.")
-            dataframes = update_database(engine)
-            df_students_clean, df_jobs_clean, df_courses = process_dataframes(dataframes)
-            update_db_table(engine, df_students_clean)
-            merged_df = merge_df(df_students_clean, df_jobs_clean, df_courses)
-            merged_df.to_csv('merged_data.csv', index=False)
-            logging.info("Data processing and merging completed successfully.")
-    except Exception as e:
-        errorlog.error(f"An error occurred: {e}")
-        raise
+	# Run tests before pipeline execution
+	if not run_tests():
+		sys.exit("Tests failed. Aborting pipeline execution.")
+
+	# Pipeline logic
+	changelog.info("Running pipeline...")
+
+	engine = create_db_engine(db_url)
+	last_row_counts = load_row_counts(row_counts_file)
+	try:
+		if not check_for_updates(engine, last_row_counts):
+			changelog.info("No updates found in the database.")
+		else:
+			changelog.info("Updates found in the database.")
+			dataframes = get_dataframes(engine)
+			df_students, df_jobs, df_courses = process_dataframes(dataframes)
+			update_db_tables(engine, df_students, df_jobs, df_courses)
+
+			current_row_counts = get_row_counts(engine)
+			save_row_counts(current_row_counts, row_counts_file)
+			changelog.info(f"Row count after changes: {current_row_counts}")
+
+			merged_df = merge_df(df_students, df_jobs, df_courses)
+			merged_df.to_csv('merged_data.csv', index=False)
+			changelog.info("Data processing and merging completed successfully.")
+	except Exception as e:
+		errorlog.error(f"An error occurred: {e}")
+		raise
 
 if __name__ == "__main__":
     main()
--- tests/tests.py
+++ tests/tests.py
@@ -1,36 +1,196 @@
 import unittest
-from your_script_name import create_db_connection, check_for_updates, update_database, process_dataframes, save_dataframes
 import sqlalchemy
+import pandas as pd
+import os
+import sys
+from sqlalchemy import create_engine, text
+from unittest import TestCase, mock
+import logging
 
-class TestDatabaseFunctions(unittest.TestCase):
-    
-    def setUp(self):
-        self.engine = create_db_connection("sqlite:///test_cademycode.db")
-        # Initialize test database state
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+test_logger = logging.getLogger('testlog')
 
-    def test_create_db_connection(self):
-        self.assertIsInstance(self.engine, sqlalchemy.engine.base.Engine)
+# Create a formatter with timestamp
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-    def test_check_for_updates(self):
-        last_row_counts = {"test_table": 10}
-        self.assertFalse(check_for_updates(self.engine, last_row_counts))
+# Create file handler and set formatter
+test_handler = logging.FileHandler('../testlog.log')
+test_handler.setFormatter(formatter)
 
-    def test_update_database(self):
-        dataframes = update_database(self.engine)
-        self.assertIsInstance(dataframes, dict)
+# Add handler to the logger
+test_logger.addHandler(test_handler)
 
-    def test_process_dataframes(self):
-        dataframes = update_database(self.engine)
-        df_dropped = process_dataframes(dataframes)
-        self.assertNotIn('NaN', df_dropped.values)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../dev')))
 
-    def test_save_dataframes(self):
-        dataframes = update_database(self.engine)
-        df_dropped = process_dataframes(dataframes)
-        try:
-            save_dataframes(self.engine, df_dropped)
-        except Exception as e:
-            self.fail(f"save_dataframes raised an exception {e}")
+from pipeline import create_db_engine, get_table_names, get_dataframes, \
+						process_dataframes, update_db_tables, merge_df, check_for_updates, get_row_counts, check_for_updates
+
+
+class TestPipelineFunctions(unittest.TestCase):
+
+	def setUp(self):
+		self.db_url = "sqlite:///../dev/cademycode.db"
+		self.engine = create_db_engine(self.db_url)
+		with self.engine.connect() as connection:
+			connection.execute(text("CREATE TABLE IF NOT EXISTS test_table (id INTEGER PRIMARY KEY, name TEXT)"))
+			connection.execute(text("INSERT INTO test_table (name) VALUES ('test_name')"))
+		self.last_row_counts = {
+			'cademycode_courses': 10,
+			'cademycode_student_jobs': 20,
+			'cademycode_students': 30
+		}
+		test_logger.info("Setup for tests completed")
+
+
+	def tearDown(self):
+		with self.engine.connect() as connection:
+			connection.execute(text("DROP TABLE IF EXISTS test_table"))
+		self.engine.dispose()
+
+
+	def test_create_db_engine(self):
+		with self.engine.connect() as connection:
+			query = text("SELECT 1")
+			result = connection.execute(query).fetchone()
+			test_logger.info("Connection successful!")
+			assert result == (1, )
+
+
+	@mock.patch('pipeline.get_row_counts')
+	def test_check_for_updates_no_change(self, mock_get_row_counts):
+		test_logger.info("Starting test: test_check_for_updates_no_change")
+
+		# Mock the get_row_counts function to return the same counts
+		mock_get_row_counts.return_value = {
+			'cademycode_courses': 10,
+			'cademycode_student_jobs': 20,
+			'cademycode_students': 30
+		}
+		test_logger.info(f"Last row counts: {self.last_row_counts}")
+		test_logger.info(f"Current row counts (mocked): {mock_get_row_counts.return_value}")
+
+		# Call check_for_updates with the mocked get_row_counts
+		has_updates = check_for_updates(self.engine, self.last_row_counts, mock_get_row_counts.return_value)
+
+		test_logger.info(f"Updates found: {has_updates}")
+
+		self.assertFalse(has_updates, f"Expected False but got True for has_updates. Last counts: {self.last_row_counts}")
+
+
+	@mock.patch('pipeline.get_row_counts')
+	def test_check_for_updates_with_change(self, mock_get_row_counts):
+		test_logger.info("Starting test: test_check_for_updates_with_change")
+
+		# Mock the get_row_counts function to return different counts
+		mock_get_row_counts.return_value = {
+			'cademycode_courses': 11,  # Change in the row count
+			'cademycode_student_jobs': 20,
+			'cademycode_students': 30
+		}
+
+		# Log the values before invoking check_for_updates
+		test_logger.info(f"Last row counts: {self.last_row_counts}")
+
+		has_updates = check_for_updates(self.engine, self.last_row_counts, mock_get_row_counts.return_value)
+
+		test_logger.info(f"Updates found: {has_updates}")
+		self.assertTrue(has_updates, f"Expected True but got False for has_updates. Last counts: {self.last_row_counts}")
+
+
+	@mock.patch('pipeline.get_row_counts')
+	def test_check_for_updates_new_table(self, mock_get_row_counts):
+		# Mock the get_row_counts function to include a new table
+		mock_get_row_counts.return_value = {
+			'cademycode_courses': 10,
+			'cademycode_student_jobs': 20,
+			'cademycode_students': 30,
+			'new_table': 5  # New table added
+		}
+
+		has_updates = check_for_updates(self.engine, self.last_row_counts, mock_get_row_counts.return_value)
+		self.assertTrue(has_updates)
+
+
+	def test_get_table_names(self):
+		with self.engine.connect() as connection:
+			connection.execute(text("CREATE TABLE IF NOT EXISTS test_table (id INTEGER PRIMARY KEY, name TEXT)"))
+			connection.execute(text("INSERT INTO test_table (name) VALUES ('test_name')"))
+		table_names = get_table_names(self.engine)
+		test_logger.info(f"table names: {table_names}")
+		self.assertIn('test_table', table_names)
+
+
+	def test_get_dataframes(self):
+		dataframes = get_dataframes(self.engine)
+		print("DataFrames loaded from the database:")	
+		for table_name, df in dataframes.items():
+			test_logger.info(f"Table: {table_name}")
+			self.assertIsInstance(df, pd.DataFrame, f"{table_name} should be a DataFrame")
+
+
+	def test_df_students_not_empty(self):
+		dataframes = get_dataframes(self.engine)
+		df_students = dataframes.get('cademycode_students', pd.DataFrame())
+		test_logger.info(f"dataframe df_students:  {df_students.head()}")
+		self.assertFalse(df_students.empty, f"{df_students} should not be empty")
+
+
+	def test_df_students_nan_dropped(self):
+		dataframes = get_dataframes(self.engine)
+		df_students, df_jobs, df_courses = process_dataframes(dataframes)
+		nan_counts = df_students.isna().sum()
+		test_logger.info(f"NaN dropped count: {nan_counts}")
+		assert (nan_counts == 0).all()
+
+
+	def test_df_students_dtypes(self):
+		dataframes = get_dataframes(self.engine)
+		df_students, df_jobs, df_courses = process_dataframes(dataframes)
+		# Check data types of columns
+		assert df_students['job_id'].dtype in ['int32', 'int64'], "Column 'job_id' should have int64 data type"
+		assert df_students['num_course_taken'].dtype in ['int32', 'int64'], "Column 'num_course_taken' should have int64 data type"
+		assert df_students['time_spent_hrs'].dtype == 'float64', "Column 'time_spent_hrs' should have float64 data type"
+		assert df_students['contact_info'].dtype == 'object', "Column 'contact_info' should have object (string) data type"
+		assert df_students['current_career_path_id'].dtype in ['int32', 'int64'], "Column 'current_career_path_id' should have int64 data type"
+
+
+	def test_df_jobs_not_empty(self):
+		dataframes = get_dataframes(self.engine)
+		df_jobs = dataframes.get('cademycode_student_jobs', pd.DataFrame())
+		test_logger.info(f"dataframe df_students:  {df_jobs.head()}")
+		self.assertFalse(df_jobs.empty, f"{df_jobs} should not be empty")
+
+
+	def test_df_jobs_duplicated_dropped(self):
+		dataframes = get_dataframes(self.engine)
+		df_students, df_jobs, df_courses = process_dataframes(dataframes)
+		duplicates = df_jobs.duplicated()
+		test_logger.info(f"df_jobs duplicates: {duplicates}")
+		assert not duplicates.any(), "There are duplicate rows in the DataFrame"
+
+
+	def test_merged_df_row_count(self):
+		dataframes = get_dataframes(self.engine)
+		df_students, df_jobs, df_courses = process_dataframes(dataframes)
+		merged_df = merge_df(df_students, df_jobs, df_courses)
+		expected_row_count = len(merged_df)
+		self.assertEqual(len(merged_df), expected_row_count)
+
+
+	def test_merged_df_columns(self):     
+		# Check columns
+		dataframes = get_dataframes(self.engine)
+		df_students, df_jobs, df_courses = process_dataframes(dataframes)
+		merged_df = merge_df(df_students, df_jobs, df_courses)
+
+		expected_columns = ['uuid', 'name', 'dob', 'sex', 'contact_info', 'job_id',
+		'num_course_taken', 'current_career_path_id', 'time_spent_hrs',
+		'job_category', 'avg_salary', 'career_path_id', 'career_path_name',
+		'hours_to_complete']
+
+		self.assertListEqual(list(merged_df.columns), expected_columns)
+        
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
